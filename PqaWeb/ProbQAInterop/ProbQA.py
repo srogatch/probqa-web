1efd509395a6ce831e8e0d842341aa1e5447e913
Serge Rogatch <serge.rogatch@gmail.com>
2018-12-31 03:27:43 +0300

    Implemented Python interop for logger initialization.

--- PqaWeb/ProbQAInterop/ProbQA.py
+++ PqaWeb/ProbQAInterop/ProbQA.py
@@ -1,6 +1,9 @@
 import os
 import ctypes
 
+# Initialization and C wrapper follow: please, don't use them in Python code.
+# Instead, please use OOP wrapper that follows later.
+
 # Taken from https://stackoverflow.com/questions/7586504/python-accessing-dll-using-ctypes
 if os.name == 'nt':
     from ctypes import wintypes
@@ -53,5 +56,101 @@ LOAD_LIBRARY_SEARCH_DEFAULT_DIRS    = 0x00001000
 #TODO: on Linux that would be a .so, but the engine doesn't yet support Linux
 pqa_engine_path = os.path.realpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "DLLs/PqaCore.dll"))
 print('Initializing engine from:', pqa_engine_path)
-pqa_lib = CDLLEx(pqa_engine_path, LOAD_WITH_ALTERED_SEARCH_PATH)
+pqa_core = CDLLEx(pqa_engine_path, LOAD_WITH_ALTERED_SEARCH_PATH)
 
+# See https://github.com/srogatch/ProbQA/blob/master/ProbQA/PqaCore/Interface/PqaCInterop.h
+class CiEngineDefinition(ctypes.Structure):
+    _pack_ = 8
+    _fields_ = [
+        ('nAnswers', ctypes.c_int64),
+        ('nQuestions', ctypes.c_int64),
+        ('nTargets', ctypes.c_int64),
+        ('precType', ctypes.c_uint8),
+        ('precExponent', ctypes.c_uint16),
+        ('precMantissa', ctypes.c_uint32),
+        ('initAmount', ctypes.c_double),
+        ('_memPoolMaxBytes', ctypes.c_uint64),
+    ]
+
+class CiAnsweredQuestion(ctypes.Structure):
+    _pack_ = 8
+    _fields_ = [
+        ('iQuestion', ctypes.c_int64),
+        ('iAnswer', ctypes.c_int64),
+    ]
+
+class CiEngineDimensions(ctypes.Structure):
+    _pack_ = 8
+    _fields_ = [
+        ('nAnswers', ctypes.c_int64),
+        ('nQuestions', ctypes.c_int64),
+        ('nTargets', ctypes.c_int64),
+    ]
+
+class CiRatedTarget(ctypes.Structure):
+    _pack_ = 8
+    _fields_ = [
+        ('iTarget', ctypes.c_int64),
+        ('prob', ctypes.c_double),
+    ]
+
+# PQACORE_API uint8_t CiLogger_Init(void **ppStrErr, const char* baseName);
+pqa_core.CiLogger_Init.restype = ctypes.c_uint8
+pqa_core.CiLogger_Init.argtypes = (ctypes.POINTER(ctypes.c_char_p), ctypes.c_char_p)
+
+# PQACORE_API void CiReleaseString(void *pvString);
+pqa_core.CiReleaseString.restype = None
+pqa_core.CiReleaseString.argtypes = (ctypes.c_char_p,)
+
+# PQACORE_API void* CiPqaGetEngineFactory();
+# PQACORE_API void* CiPqaEngineFactory_CreateCpuEngine(void* pvFactory, void **ppError, CiEngineDefinition *pEngDef);
+# PQACORE_API void* CiqaEngineFactory_LoadCpuEngine(void *pvFactory, void **ppError, const char* filePath,
+#   uint64_t memPoolMaxBytes);
+
+# PQACORE_API void CiReleasePqaError(void *pvErr);
+# PQACORE_API void* CiPqaError_ToString(void *pvError, const uint8_t withParams);
+
+# PQACORE_API void CiReleasePqaEngine(void *pvEngine);
+# PQACORE_API void* PqaEngine_Train(void *pvEngine, int64_t nQuestions, const CiAnsweredQuestion* const pAQs,
+#   const int64_t iTarget, const double amount = 1.0);
+# PQACORE_API uint8_t PqaEngine_QuestionPermFromComp(void *pvEngine, const int64_t count, int64_t *pIds);
+# PQACORE_API uint8_t PqaEngine_QuestionCompFromPerm(void *pvEngine, const int64_t count, int64_t *pIds);
+# PQACORE_API uint8_t PqaEngine_TargetPermFromComp(void *pvEngine, const int64_t count, int64_t *pIds);
+# PQACORE_API uint8_t PqaEngine_TargetCompFromPerm(void *pvEngine, const int64_t count, int64_t *pIds);
+# PQACORE_API uint64_t PqaEngine_GetTotalQuestionsAsked(void *pvEngine, void **ppError);
+# PQACORE_API uint8_t PqaEngine_CopyDims(void *pvEngine, CiEngineDimensions *pDims);
+# PQACORE_API int64_t PqaEngine_StartQuiz(void *pvEngine, void **ppError);
+# PQACORE_API int64_t PqaEngine_ResumeQuiz(void *pvEngine, void **ppError, const int64_t nAnswered,
+#   const CiAnsweredQuestion* const pAQs);
+# PQACORE_API int64_t PqaEngine_NextQuestion(void *pvEngine, void **ppError, const int64_t iQuiz);
+# PQACORE_API void* PqaEngine_RecordAnswer(void *pvEngine, const int64_t iQuiz, const int64_t iAnswer);
+# PQACORE_API int64_t PqaEngine_GetActiveQuestionId(void *pvEngine, void **ppError, const int64_t iQuiz);
+# PQACORE_API int64_t PqaEngine_ListTopTargets(void *pvEngine, void **ppError, const int64_t iQuiz,
+#   const int64_t maxCount, CiRatedTarget *pDest);
+# PQACORE_API void* PqaEngine_RecordQuizTarget(void *pvEngine, const int64_t iQuiz, const int64_t iTarget,
+#   const double amount = 1.0);
+# PQACORE_API void* PqaEngine_ReleaseQuiz(void *pvEngine, const int64_t iQuiz);
+# PQACORE_API void* PqaEngine_SaveKB(void *pvEngine, const char* const filePath, const uint8_t bDoubleBuffer);
+
+# OOP wrapper follows - please, use these in Python code
+class PqaException(Exception):
+    pass
+
+class Utils:
+    def handle_native_string(c_str : ctypes.c_char_p) -> str:
+        ans = c_str.value.decode('ascii')
+        pqa_core.CiReleaseString(c_str)
+        return ans
+
+class SRLogger:
+    def init(base_name : str) -> bool:
+        str_err = ctypes.c_char_p()
+        ans = (pqa_core.CiLogger_Init(ctypes.byref(str_err), ctypes.c_char_p(base_name.encode('ascii'))) != 0)
+        if str_err:
+            raise PqaException(Utils.handle_native_string(str_err))
+
+class PqaEngine:
+    # Permanent-compact ID mappings follow
+    def question_perm_from_comp(ids : list) -> bool:
+        # https://stackoverflow.com/questions/37197631/ctypes-reading-modified-array
+        pass
--- PqaWeb/pqawV1/apps.py
+++ PqaWeb/pqawV1/apps.py
@@ -1,4 +1,6 @@
+import os
 from django.apps import AppConfig
+from django.conf import settings
 import ProbQAInterop.ProbQA as probqa
 
 class Pqawv1Config(AppConfig):
@@ -7,5 +9,7 @@ class Pqawv1Config(AppConfig):
     # https://stackoverflow.com/questions/6791911/execute-code-when-django-starts-once-only
     def ready(self):
         print('Startup of subsystems...')
+        # Init SRLogger
+        probqa.SRLogger.init(os.path.join(settings.BASE_DIR, '../../logs/PqaWeb'))
         # Load ProbQA engine
         print('All subsystems operational!')
